91ff297f96d7fb877aed8f5036a4af061d0efd65
Majid Ali Khan <51267704+majidalykhan@users.noreply.github.com>
2021-07-01 23:20:10 +0500

    Their Message

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,11 +1,11 @@
 const MyMessage = ({message}) => {
     if(message?.attachments?.length > 0){
         return (
-            <img src={message.attachments[0].file} alt='message-attachment' className='message-image' style={{float:right}} />
+            <img src={message.attachments[0].file} alt='message-attachment' className='message-image' style={{float:'right'}} />
         )
     }
     return (
-        <div className='message' style={{float:right, marginRight:'18px', color:'white', backgroundColor:'red'}}>
+        <div className='message' style={{float:'right', marginRight:'18px', color:'white', backgroundColor:'red'}}>
             {message.text}   
         </div>
     )
