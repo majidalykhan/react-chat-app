e9e2d0470b92c2da6789136e009d8dc596b318ea
Majid Ali Khan <51267704+majidalykhan@users.noreply.github.com>
2021-07-01 20:45:41 +0500

    Render message function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 
+import ChatFeed from "./components/ChatFeed";
+
 function App() {
   const PROJECT_ID = process.env.REACT_APP_CHAT;
   const USER_SECRET = process.env.REACT_APP_USERSECRET;
@@ -10,6 +12,7 @@ function App() {
       projectID={PROJECT_ID}
       userName="majidalykhan"
       userSecret={USER_SECRET}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 }
