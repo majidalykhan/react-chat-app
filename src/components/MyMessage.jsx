02327dd262315632f3c1561f584814d288045926
Majid Ali Khan <51267704+majidalykhan@users.noreply.github.com>
2021-07-01 23:07:38 +0500

    MyMessage

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,8 +1,12 @@
-const MyMessage = () => {
-
+const MyMessage = ({message}) => {
+    if(message?.attachments?.length > 0){
+        return (
+            <img src={message.attachments[0].file} alt='message-attachment' className='message-image' style={{float:right}} />
+        )
+    }
     return (
-        <div>
-            MyMessage
+        <div className='message' style={{float:right, marginRight:'18px', color:'white', backgroundColor:'red'}}>
+            {message.text}   
         </div>
     )
 
